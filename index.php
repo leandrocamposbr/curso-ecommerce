834164fc20c44f70d8c33f69ac8479281188b2dd
Leandro Campos <leandrocamposbr@hotmail.com>
2022-01-03 21:17:33 -0300

    criada a classe Page

--- index.php
+++ index.php
@@ -2,22 +2,39 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+// informa os namespaces. Dentro de vendor tem vários. Aqui informe o que quer carregar
+use \Hcode\Page; // aula 103 nosso namespace. Dentro de vendor tem várias. Essa é a nossa.
+use \Slim\Slim;	 
+
+// aula 103 - aqui cria 'uma nova aplicação' no slim, para as rotas.
+// antes chamávamos 'index.php, cadastro.php, xpto.php' com atributos na url.
+// hoje, devido a SEO, rankeamento de busca etc, se usa rota...
+// ... Passe um nome na URL e ele me manta para algum lugar(!)
+// é isso o que o slim faz. (veja*1)
+$app = new Slim();	// para instanciar direto sem 'use' seria '$app = new \Slim\Slim();'
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+// "/" é a rota que estamos chamando e o bloco da rota.
+// (*1) aula 103 - "se chamarem meu site sem nenhum complemento na url ("/"), execute isso"
+$app->get('/', function() {		
     
 	// Leoc - original
 	//echo "OK";
 
 	// leoc - aula 102 depois de incluir a classe sql com namespace Hcode\DB
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	//$sql = new Hcode\DB\Sql();
+	//$results = $sql->select("SELECT * FROM tb_users");
+	//echo json_encode($results);
+
+	// aula 103, depois de criar a classe Page.
+	$page = new Page(); // aqui já vai incluir o header
+	$page->setTpl("index"); // acrescenta o corpo
+
+	// nesse ponto, vai chamar o __destruct() da classe e desenhar o footer
 
 });
 
-$app->run();
+$app->run(); // aqui é que executa tudo acima.
 
  ?>
---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+// leoc - baseado no 'example-simple.php da pasta do raintpl, conforme aula 103.
+
+class Page {
+
+    // leoc - aula 103 - as variáveis (parâmetros na chamada da classe) virão de acordo com a rota chamada no slim.
+
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data"=>[]
+    ];    
+
+    // leoc - __construct é um método mágico.
+    public function __construct($opts = array()) {
+     
+        // leoc - array_merge mescla as arrays (!). A ordem do merge é importante. O último parâmetro $opts, 
+        // se vier no construtor, e algo nele der conflito com o $defaults, o que vale é $opts, que sobrepõe o conflito no $default 
+        $this->options = array_merge($this->defaults, $opts);
+
+       	
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+            "debug"         => false // set to false to improve the speed
+       );
+
+       Tpl::configure( $config );
+
+       $this->tpl = new Tpl;
+
+       // leoc - dados estão na chave "data". 
+       $this->setData($this->options["data"]);
+
+       // leoc - desenhar cabeçalho que se repete em toda página, sempre que instanciar esta classe Page.
+       $this->tpl->draw("header"); // o método espera o nome do arquivo (que está em tpl_dir)
+       
+    }
+
+    // método otimizado para percorrer o array no parâmetro recebido. Feito assim para não repetir o código no __construct() e no setTpl()
+    private function setData($data = array()) {
+        foreach ($data as $key => $value) {
+            $this->tpl->assign($key, $value);
+        }
+    }
+
+    // corpo da página
+    public function setTpl($name, $data = array(), $returnHTML = false) {
+        
+        $this->setData($data);
+
+        return $this->tpl->draw($name, $returnHTML); // return apenas se precisar retornar. Não obrigatório.
+    }
+
+    public function __destruct() {
+ 
+        // rodapé que se repete em todas as páginas
+        $this->tpl->draw("footer"); // o método espera o nome do arquivo (que está em tpl_dir)
+
+    }
+
+}
+
+
+?>
